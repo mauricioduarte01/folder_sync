4920c6a50510d68dabd88a39574010ea931287c7
mauriciod <mauricioduarte01@gmail.com>
2023-08-18 19:56:25 +0200

    dumb update

--- dumb_sync.sh
+++ dumb_sync.sh
@@ -4,5 +4,6 @@
 SOURCE_DIR="/home/bladerunner/Documents/folder1"
 DEST_DIR="/home/bladerunner/Documents/folder2"
 
-# Sync the files from source to destination
-rsync -avz --delete "$SOURCE_DIR/" "$DEST_DIR/"
+# Sync the files from source to destination, excluding specific files
+rsync -avz --delete --exclude='subdir/CMakeLists.txt' "$SOURCE_DIR/" "$DEST_DIR/"
+
